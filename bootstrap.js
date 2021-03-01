9b5818589f443904f16b0f9f09f7d5b026496552
Kamil Szubiński <szubinskik@gmail.com>
2021-03-01 23:07:57 +0100

    Updates

--- bootstrap.js
+++ bootstrap.js
@@ -240,7 +240,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_pie_bg.wasm":"3f8d5a0edf1c32f86a77"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_pie_bg.wasm":"e73986d21e59a51ae6f1"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
